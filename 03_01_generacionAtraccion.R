34847b7999869d1c33ef0ad25834debb3360e69d
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2019-09-03 19:46:40 -0500

    Generacion y atraccion
    Comunas

--- 03_01_generacionAtraccion.R
+++ 03_01_generacionAtraccion.R
@@ -14,6 +14,8 @@ ori <- shapefile('../data/shp/own/movilidad/geocoding/oneByone/8-26_Origen.shp')
 dst <- shapefile('../data/shp/own/movilidad/geocoding/oneByone/8-26_Destino.shp')
 adm <- shapefile('../data/shp/base/bcs_barrios_geo.shp')
 com <- shapefile('../data/shp/base/bcs_comunas_geo.shp')
+lfm.ori <- st_read('../data/shp/own/movilidad/lfm/03_origen_viajes.shp')
+lfm.dst <- st_read('../data/shp/own/movilidad/lfm/04_destino_viajes.shp')
 
 # Interseccion entre el shape de destino y el shape de barrios a nivel general
 dst <- raster::intersect(dst, adm)
@@ -52,12 +54,38 @@ smm_ori <- ori %>%
   ungroup() %>% 
   arrange(desc(CONTEO))
 
+######
+lfm.ori <- lfm.ori %>% 
+  dplyr::select(ID_COMUNA, TOT_VIAJES) %>% 
+  as.data.frame() %>% 
+  group_by(ID_COMUNA) %>% 
+  dplyr::summarise(count = n()) %>% 
+  ungroup() %>% 
+  setNames(c('COMUNA', 'CONTEO'))
+lfm.dst <- lfm.dst %>% 
+  dplyr::select(ID_COMUNA, TOTAL_VIAJ) %>% 
+  as.data.frame() %>% 
+  group_by(ID_COMUNA) %>% 
+  dplyr::summarise(count = n()) %>% 
+  ungroup() %>% 
+  setNames(c('COMUNA', 'CONTEO'))
+
+smm_ori <- rbind(lfm.ori, smm_ori)
+smm_dst <- rbind(lfm.dst, smm_dst) %>% 
+  group_by(COMUNA) %>% 
+  dplyr::summarise(count = sum(CONTEO)) %>% 
+  ungroup()
+
 # Union de la tabla resumen con el shapefile de comunas
 com <- st_as_sf(com)
-
 com_dst <- inner_join(x = com, y = smm_dst, by = c('COMUNA' = 'COMUNA'))
 com_dst <- as(com_dst, 'Spatial')
 com_ori <- inner_join(x = com, y = smm_ori, by = c('COMUNA' = 'COMUNA'))
-com_ori <- as(com_fnl, 'Spatial')
+com_ori <- as(com_ori, 'Spatial')
 
 # Mapa de generacion de las comuna con mas atraccion (Comuna 19)
+dir.create('../data/shp/own/movilidad/analysis')
+writeOGR(obj = com_dst, dsn = '../data/shp/own/analysis', layer = 'comunas_destino', driver = 'ESRI Shapefile', overwrite_layer = TRUE)
+writeOGR(obj = com_ori, dsn = '../data/shp/own/analysis', layer = 'comunas_origen', driver = 'ESRI Shapefile', overwrite_layer = TRUE)
+
+
